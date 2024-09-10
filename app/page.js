089bdc7b35e41cc2797a881fd42f46386863613e
talkin-ai <126031354+talkin-ai@users.noreply.github.com>
2024-09-09 23:09:55 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRef, useState, useEffect } from "react";
+import Head from 'next/head';
 
 export default function Home() {
   const voiceRef = useRef();
@@ -75,15 +76,36 @@ export default function Home() {
   }, []);
 
   return (
-    <main>
-      <div className="bg-white py-4 px-4 rounded-md">
-        
-     <h2 className="relative text-2xl font-semibold tracking-tight border-b sm:text-3xl scroll-m-20 first:mt-0">
-        <span className="text-3xl font-extrabold text-primary">AI</span> Voice
-        Generator
-      </h2>
-   
+    
+    <>
+            <Head>
+                <title>AIVIDOO AI Voice Generator</title>
 
+                <meta name="description" content="AIVIDOO AI voice generator" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                <link rel="icon" href="/aividoo_logo.png" />
+            </Head>
+
+            
+               
+
+            <main className="p-10 mx-auto max-w-4xl">
+      
+
+      <div className="relative pt-20 ml-auto">
+      <div className="inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+      <div className="blur-[106px] h-20 bg-gradient-to-r from-yellow-400 to-orange-600 dark:to-indigo-600"></div>
+      <div className="blur-[106px] h-30 bg-gradient-to-r from-orange-600 to-red-400 dark:from-red-700"></div>
+      </div>
+          <div className="lg:w-3/4 text-center mx-auto mb-10">
+              <h1 className="text-white dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">AI Voice<span className="text-primary dark:text-white"> Generator.</span></h1>
+            
+          </div>
+      </div>
+    
+    
+    
         <div className="my-6 flex flex-col gap-4">
           <div className="flex gap-4 items-center">
             <label>Select a Voice:</label>
@@ -115,8 +137,9 @@ export default function Home() {
           {audioURL &&  (<audio controls autoPlay>
           <source src={audioURL} type="audio/mpeg" />
         </audio>)}
-        </div>
+        
       </div>
     </main>
+            </>
   );
 }
